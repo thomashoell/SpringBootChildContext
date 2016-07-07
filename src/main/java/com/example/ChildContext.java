38b7ed5fca52d668f10fc1d434cca74f2e453760
Thomas Höll <thomas.hoell@motionware.de>
2016-07-07 14:45:39 +0200

    * added properties for child context

--- src/main/java/com/example/ChildContext.java
+++ src/main/java/com/example/ChildContext.java
@@ -1,13 +1,11 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.PropertyPlaceholderAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
@@ -16,13 +14,17 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.example")
+@PropertySource("child-context.properties")
 @Import({PropertyPlaceholderAutoConfiguration.class, EmbeddedServletContainerAutoConfiguration.class, DispatcherServletAutoConfiguration.class})
 public class ChildContext {
+
+    @Value("${server.port}")
+    private Integer port;
 
     @Bean
     public ServerProperties serverProperties() {
         ServerProperties props = new ServerProperties();
-        props.setPort(9091);
+        props.setPort(port);
         return props;
     }
 }
